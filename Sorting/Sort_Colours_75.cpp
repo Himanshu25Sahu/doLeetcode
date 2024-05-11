8c1d8e65c29a0044221c6d7ee6220178ea380adc
Himanshu Sahu <128088575+Himanshu25Sahu@users.noreply.github.com>
2024-05-11 09:13:48 +0530

    Create Sort_Colours_75.cpp

---
+++ Sorting/Sort_Colours_75.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    
+    void sortColors(vector<int>& nums) {
+        int i = 0;
+        int j = nums.size() - 1;
+        int k = 0;
+        while (k <= j) {
+            if (nums[k] == 0) {
+                swap(nums[i], nums[k]);
+                i++;
+                k++;
+            } else if (nums[k] == 2) {
+                swap(nums[j], nums[k]);
+                j--;
+            } else {
+                k++;
+            }
+        }
+
+
+      }
+};
