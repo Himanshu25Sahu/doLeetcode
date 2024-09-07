4edead84160052ba18f06ebe236308971ee87f4b
Himanshu Sahu <128088575+Himanshu25Sahu@users.noreply.github.com>
2024-09-07 11:38:26 +0530

    Create Linked_List_in_Binary_Tree_1367.cpp

---
+++ Tree + Linked List /Linked_List_in_Binary_Tree_1367.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool check(ListNode* head, TreeNode* root) {
+
+        if (!head)
+            return true;
+        if (!root)
+            return false;
+
+        if (head->val != root->val)
+            return false;
+
+        return check(head->next, root->left) || check(head->next, root->right);
+    }
+
+    bool isSubPath(ListNode* head, TreeNode* root) {
+        if (!root)
+            return false; 
+
+        return check(head, root) || isSubPath(head, root->left) ||
+               isSubPath(head, root->right);
+    }
+};
